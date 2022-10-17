bead340ef6b91ea30a4b9b1e345438b409157efe
Weirong Wu <weirongw@umich.edu>
2022-10-16 20:23:13 -0400

    added breadth-first search algorithm for C++

--- snippets/bfs.cpp
+++ snippets/bfs.cpp
@@ -1,13 +1,27 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <queue>
 
+using namespace std;
 
 // Write an algorithm to print the vertices of the graph depth-first
 // with every vertex on each line
 void dfs(unordered_map<int, vector<int>> &graph, int element) {
-  cout << element << endl;
-}
+    <<<queue<int> q;
+    unordered_set<int> s;
+    s.insert(element);
+    q.push(element);
 
-int main() {
-  unordered_map<int, vector<int>> graph;
+    while (!q.empty()) {
+        int vertex = q.front();
+        q.pop();
 
-  
+        for (auto edge : graph[vertex]) {
+            if (s.find(edge) == s.end()) {
+                s.insert(edge);
+                q.push(edge);
+            }
+        }
+    }>>>
 }
